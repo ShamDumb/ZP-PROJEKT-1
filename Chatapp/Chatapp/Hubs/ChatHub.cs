648d7b9ed3310abe4668aded0b68b4d1a23853c8
Borys Gaik <borysiak12@gmail.com>
2023-03-12 18:58:37 +0100

    Poprawki

--- Chatapp/Chatapp/Hubs/ChatHub.cs
+++ Chatapp/Chatapp/Hubs/ChatHub.cs
@@ -1,6 +1,10 @@
-﻿namespace Chatapp.Hubs
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Chatapp.Hubs
 {
     public class ChatHub
     {
+        public async Task SendMessage(MessageProcessingHandler message) =>
+            await Clients.All.SendAsync("receiveMessage", message);
     }
 }
