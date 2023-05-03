752ab457b42ef2058baf75bce7c0a78ece834ff4
Damian Dobosz <92273672+Damisseba@users.noreply.github.com>
2023-05-03 12:01:46 +0200

    usunięcie poprzedniej wersji v2

--- Chatapp/Chatapp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Chatapp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
